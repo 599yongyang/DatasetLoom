2fd7a1ca54be2d19ce945e8d32bc9cd3c32da177
Turbobot <turbobot@vercel.com>
2025-08-02 11:55:56 +0800

    feat(create-turbo): apply official-starter transform

---
+++ temp2/next.config.js
@@ -0,0 +1,26 @@
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+import '../src/env.js';
+
+/** @type {import('next').NextConfig} */
+const config = {
+    // compress: true,
+    eslint: {
+        ignoreDuringBuilds: true
+    },
+    webpack(config, { dev }) {
+        config.externals.push({
+            canvas: 'commonjs canvas'
+        });
+        if (config.cache && !dev) {
+            config.cache = Object.freeze({
+                type: 'memory'
+            });
+        }
+        return config;
+    }
+};
+
+export default config;
